6acd170654c72dc6c05315a2799591a8281396ef
DmitryKorolevich <dmitry.korolevich@gmail.com>
2015-01-30 00:01:44 +0300

    bootrstrap was updated; added bootstrap theme

--- src/VitalChoice_vNext/wwwroot/_references.js
+++ src/VitalChoice_vNext/wwwroot/_references.js
@@ -4,3 +4,4 @@
 /// <reference path="lib/jquery/js/jquery.js" />
 /// <reference path="lib/jquery-validation/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/bootstrap/js/npm.js" />
