82fb3f2c37b96dfd33601c96a7fbd11000ceef00
DmitryKorolevich <dmitry.korolevich@gmail.com>
2015-02-06 01:59:52 +0300

    working on angular client side application structure

---
+++ src/VitalChoice_vNext/App/Shared/mainMenu/controllers/mainNavigationController.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+angular.module('app.shared.mainMenu.controllers.mainNavigationController', [
+    'app.shared.mainMenu.services.mainNavigationService'
+])
+.controller('mainNavigationController', ['$scope', 'mainNavigationFactory', function ($scope, mainNavigationFactory) {
+	$scope.mainNavigationService = mainNavigationFactory;
+	/*$scope.navigation = {
+		collection: primaryNavigation.getList('order'),
+		current: null
+	};
+
+	$scope.$on('$routeChangeSuccess', function (scope, current, previous) {
+		if (!current || !current.$$route || !current.$$route.name) {
+			$scope.navigation.current = 'dashboard';
+			return;
+		}
+		$scope.navigation.current = current.$$route.name;
+	});*/
+
+}]);
