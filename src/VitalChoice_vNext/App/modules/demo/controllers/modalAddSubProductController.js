9a445c81f1846fed0067fe1b2f322bc22dd45c61
DmitryKorolevich <dmitry.korolevich@gmail.com>
2015-03-24 09:03:49 +0300

    working on ui

---
+++ src/VitalChoice_vNext/App/modules/demo/controllers/modalAddSubProductController.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+angular.module('app.modules.demo.controllers.modalAddSubProductController', [])
+.controller('modalAddSubProductController', ['$scope', '$modalInstance', 'items', function ($scope, $modalInstance, items) {
+	$scope.items = items;
+	$scope.selected = {
+		item: $scope.items[0]
+	};
+
+	$scope.ok = function () {
+		$modalInstance.close($scope.selected.item);
+	};
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+}]);
