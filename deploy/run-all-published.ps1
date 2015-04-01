2c1b8fe772d9437f3e3c8ca71d88e09afa76135b
Aliaksandr Kukrash <multiarc@gmail.com>
2015-04-01 14:50:02 +0300

    Fix run all

--- deploy/run-all-published.ps1
+++ deploy/run-all-published.ps1
@@ -1,11 +1,10 @@
-﻿. ".\functions.ps1"
-
-$rootDeploy = "c:\inetpub\wwwroot\vitalchoice_new"
+﻿$rootDeploy = "c:\inetpub\wwwroot\vitalchoice_new"
 
 ls -Path $rootDeploy | `
 foreach{
 	if ($_.GetType().Name.Equals("DirectoryInfo") -And -Not ($_.Name.Equals("log"))) {
-		Push-Location "${rootDeploy}\" + $_.Name + "\"
+		$target = $_.Name
+		Push-Location "${rootDeploy}\${target}\"
 		start kestrel
 		Pop-Location
 	}
