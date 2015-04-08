a8d923a8c19ff35a56e6edc426047281a6adc45e
Aliaksandr Kukrash <multiarc@gmail.com>
2015-04-08 19:21:36 +0300

    Added ttl extension for ace

--- src/VC.Admin/gruntfile.js
+++ src/VC.Admin/gruntfile.js
@@ -87,7 +87,8 @@ module.exports = function (grunt) {
 				  { expand: true, cwd: 'temp/css/', src: ['**'], dest: 'wwwroot/assets/styles/' },
 				  { expand: true, cwd: 'temp/boostrap/', src: ['**'], dest: 'wwwroot/lib/bootstrap/css/' },
 				  { expand: true, cwd: 'assets/images/', src: ['**'], dest: 'wwwroot/assets/images/' },
-				  { expand: true, cwd: 'assets/fonts/', src: ['**'], dest: 'wwwroot/lib/fonts/' }
+				  { expand: true, cwd: 'assets/fonts/', src: ['**'], dest: 'wwwroot/lib/fonts/' },
+                  { expand: true, cwd: 'app/core/utils/ace/', src: ['**'], dest: 'wwwroot/lib/ace-builds/src-min-noconflict/' }
         		]
         	},
         	release: {
@@ -96,7 +97,7 @@ module.exports = function (grunt) {
 			        { expand: true, cwd: 'temp/css/minified/', src: ['**'], dest: 'wwwroot/assets/styles/' },
 					{ expand: true, cwd: 'temp/boostrap/minified/', src: ['**'], dest: 'wwwroot/lib/bootstrap/css/' },
 					{ expand: true, cwd: 'assets/images/', src: ['**'], dest: 'wwwroot/assets/images/' },
-					{ expand: true, cwd: 'assets/fonts/', src: ['**'], dest: 'wwwroot/lib/fonts/' }
+					{ expand: true, cwd: 'app/core/utils/ace/', src: ['**'], dest: 'wwwroot/lib/ace-builds/src-min-noconflict/' }
 		        ]
 	        }
         },
