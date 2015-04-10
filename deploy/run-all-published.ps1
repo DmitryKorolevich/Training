cd1af5087618afebcee63854a403548889f55725
Aliaksandr Kukrash <multiarc@gmail.com>
2015-04-10 15:28:37 +0300

    Test logs for running instances

--- deploy/run-all-published.ps1
+++ deploy/run-all-published.ps1
@@ -6,7 +6,7 @@ foreach{
 		$target = $_.Name
 		echo "starting ${target}"
 		Push-Location "${rootDeploy}\${target}\"
-		start kestrel.cmd
+		start "kestrel.cmd > ..\..\logs\${target}.log"
 		Pop-Location
 	}
 }
