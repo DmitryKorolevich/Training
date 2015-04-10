869943dc41af51344a11f250edef4be0c48ee2e5
Aliaksandr Kukrash <multiarc@gmail.com>
2015-04-10 15:35:43 +0300

    Fix process logging

--- deploy/run-all-published.ps1
+++ deploy/run-all-published.ps1
@@ -6,7 +6,7 @@ foreach{
 		$target = $_.Name
 		echo "starting ${target}"
 		Push-Location "${rootDeploy}\${target}\"
-		start "kestrel.cmd > ..\..\logs\${target}.log"
+		start kestrel.cmd | tee "..\logs\${target}.log"
 		Pop-Location
 	}
 }
