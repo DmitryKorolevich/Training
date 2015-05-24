2b12a79841ed90249954c65ea414580f316f2403
DmitryKorolevich <dmitry.korolevich@gmail.com>
2015-05-25 02:12:24 +0300

    invalid tab selection upon click - articles

--- src/VC.Admin/App/modules/content/controllers/articleManageController.js
+++ src/VC.Admin/App/modules/content/controllers/articleManageController.js
@@ -61,7 +61,10 @@ function ($scope, $rootScope, $state, $stateParams, appBootstrap, modalUtil, con
             MasterContentItemId: 0,
             PublishedDateObject: new DateObject(currentDate),
         };
-        $scope.loaded = false;
+		$scope.detailsTab = {
+			active: true
+		};
+		$scope.loaded = false;
         $scope.forms={};
 
         $scope.save = function () {
@@ -86,7 +89,8 @@ function ($scope, $rootScope, $state, $stateParams, appBootstrap, modalUtil, con
                         errorHandler(result);
                     });
             } else {
-                $scope.forms.articleForm.submitted = true;
+            	$scope.forms.articleForm.submitted = true;
+	            $scope.detailsTab.active = true;
             }
         };
 
