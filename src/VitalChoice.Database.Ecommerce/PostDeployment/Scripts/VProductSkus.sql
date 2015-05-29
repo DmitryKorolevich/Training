1a6fc08add17819a27a0d3ddd517adbb53c8059e
Aliaksandr Kukrash <multiarc@gmail.com>
2015-05-29 13:42:05 +0300

    * View fix

--- src/VitalChoice.Database.Ecommerce/PostDeployment/Scripts/VProductSkus.sql
+++ src/VitalChoice.Database.Ecommerce/PostDeployment/Scripts/VProductSkus.sql
@@ -17,7 +17,7 @@ SELECT
 	p.Name,
 	p.Url,
 	ISNULL(val.Value, opt.DefaultValue) AS Thumbnail
-	FROM Skus AS s
-	INNER JOIN Products AS p ON p.Id = s.IdProduct
+	FROM Products AS p
+	LEFT JOIN Skus AS s ON p.Id = s.IdProduct
 	INNER JOIN ProductOptionTypes AS opt ON opt.Name = N'Thumbnail' AND opt.IdProductType = p.IdProductType
 	LEFT JOIN ProductOptionValues AS val ON val.IdProduct = p.Id AND val.IdOptionType = opt.Id
