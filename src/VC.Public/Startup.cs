83ce0889dea58d631783cff38d611d194b05abe5
Aliaksandr Kukrash <multiarc@gmail.com>
2015-06-04 12:28:51 +0300

    *DateTime formatter fixes

--- src/VC.Public/Startup.cs
+++ src/VC.Public/Startup.cs
@@ -1,95 +1,92 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Diagnostics.Entity;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Routing;
-using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNet.Mvc;
 using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using VitalChoice.Core.DependencyInjection;
-using Microsoft.AspNet.Mvc;
-using VitalChoice.Core.Infrastructure;
-using System.IO;
 using Microsoft.Framework.Runtime;
 using Newtonsoft.Json;
 using VC.Public.AppConfig;
 using VitalChoice.Business.Services;
+using VitalChoice.Core.DependencyInjection;
+using VitalChoice.Core.Infrastructure;
 
-namespace VitalChoice.Public
+namespace VC.Public
 {
-	public class Startup
-	{
-		public IConfiguration Configuration { get; set; }
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
 
 
-	    public IServiceProvider ConfigureServices(IServiceCollection services)
-	    {
-	        var applicationEnvironment = services.BuildServiceProvider().GetRequiredService<IApplicationEnvironment>();
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            var applicationEnvironment = services.BuildServiceProvider().GetRequiredService<IApplicationEnvironment>();
 
-	        var configuration = new Configuration( /*applicationEnvironment.ApplicationBasePath*/)
-	            .AddJsonFile("config.json")
-	            .AddEnvironmentVariables();
+            var configuration = new Configuration( /*applicationEnvironment.ApplicationBasePath*/)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
 
-	        var path = PathResolver.ResolveAppRelativePath("config.local.json");
-	        if (File.Exists(path))
-	        {
-	            configuration.AddJsonFile("config.local.json");
-	        }
-	        Configuration = configuration;
+            var path = PathResolver.ResolveAppRelativePath("config.local.json");
+            if (File.Exists(path))
+            {
+                configuration.AddJsonFile("config.local.json");
+            }
+            Configuration = configuration;
 
-	        //services.Configure<MvcOptions>(o =>
-	        //{
-	        //    var inputFormatter =
-	        //        (JsonInputFormatter)
-	        //            o.InputFormatters.SingleOrDefault(f => f.GetType() == typeof (JsonInputFormatter))?.Instance;
-	        //    var outputFormatter =
-	        //        (JsonOutputFormatter)
-	        //            o.OutputFormatters.SingleOrDefault(f => f.GetType() == typeof (JsonOutputFormatter))?.Instance;
+            services.Configure<MvcOptions>(o =>
+            {
+                var inputFormatter =
+                    (JsonInputFormatter)
+                        o.InputFormatters.SingleOrDefault(f => f.GetType() == typeof (JsonInputFormatter))?.Instance;
+                var outputFormatter =
+                    (JsonOutputFormatter)
+                        o.OutputFormatters.SingleOrDefault(f => f.GetType() == typeof (JsonOutputFormatter))?.Instance;
 
-	        //    if (inputFormatter != null)
-	        //    {
-	        //        inputFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-	        //        inputFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
-	        //        inputFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-	        //    }
-	        //    else
-	        //    {
-	        //        // ReSharper disable once UseObjectOrCollectionInitializer
-	        //        var newFormatter = new JsonInputFormatter();
-	        //        newFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-	        //        newFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
-	        //        newFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-	        //        o.InputFormatters.Add(newFormatter);
-	        //    }
+                if (inputFormatter != null)
+                {
+                    inputFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                    inputFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
+                    inputFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+                }
+                else
+                {
+                    // ReSharper disable once UseObjectOrCollectionInitializer
+                    var newFormatter = new JsonInputFormatter();
+                    newFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                    newFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
+                    newFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+                    o.InputFormatters.Add(newFormatter);
+                }
 
-	        //    if (outputFormatter != null)
-	        //    {
-	        //        outputFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-	        //        outputFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
-	        //        outputFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-	        //    }
-	        //    else
-	        //    {
-         //           // ReSharper disable once UseObjectOrCollectionInitializer
-         //           var newFormatter = new JsonOutputFormatter();
-	        //        newFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-	        //        newFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
-	        //        newFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-	        //        o.OutputFormatters.Add(newFormatter);
-	        //    }
-	        //});
+                if (outputFormatter != null)
+                {
+                    outputFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                    outputFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
+                    outputFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+                }
+                else
+                {
+                    // ReSharper disable once UseObjectOrCollectionInitializer
+                    var newFormatter = new JsonOutputFormatter();
+                    newFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                    newFormatter.SerializerSettings.DateParseHandling = DateParseHandling.DateTime;
+                    newFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+                    o.OutputFormatters.Add(newFormatter);
+                }
+            });
 
-	        var reg = new DefaultDependencyConfig();
-	        return reg.RegisterInfrastructure(Configuration, services, null);
-	    }
+            var reg = new DefaultDependencyConfig();
+            return reg.RegisterInfrastructure(Configuration, services, null);
+        }
 
-	    // Configure is called after ConfigureServices is called.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
-		{
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+        {
             // Configure the HTTP request pipeline.
             // Add the console logger.
             loggerfactory.AddConsole();
@@ -97,27 +94,27 @@ namespace VitalChoice.Public
 
             // Add the following to the request pipeline only in development environment.
             if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
-			{
-				app.UseErrorPage(ErrorPageOptions.ShowAll);
-				app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
-			}
-			else
-			{
-				// Add Error handling middleware which catches all application specific errors and
-				// send the request to the following path or controller action.
-				app.UseErrorHandler("/Home/Error");
-			}
+            {
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+                app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
+            }
+            else
+            {
+                // Add Error handling middleware which catches all application specific errors and
+                // send the request to the following path or controller action.
+                app.UseErrorHandler("/Home/Error");
+            }
 
-			// Add static files to the request pipeline.
-			app.UseStaticFiles();
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
 
-			// Add cookie-based authentication to the request pipeline.
-			app.UseIdentity();
+            // Add cookie-based authentication to the request pipeline.
+            app.UseIdentity();
 
             app.UseMvc(routes =>
-			{
+            {
                 RouteConfig.RegisterRoutes(routes);
-			});
-		}
-	}
+            });
+        }
+    }
 }
