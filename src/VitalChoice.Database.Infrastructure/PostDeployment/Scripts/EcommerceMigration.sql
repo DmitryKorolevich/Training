7ca475f99058cd62ec79ebacdb3f1a50d3c6d3a6
DmitryKorolevich <dmitry.korolevich@gmail.com>
2015-06-15 03:16:31 +0300

    -

--- src/VitalChoice.Database.Infrastructure/PostDeployment/Scripts/EcommerceMigration.sql
+++ src/VitalChoice.Database.Infrastructure/PostDeployment/Scripts/EcommerceMigration.sql
@@ -1,11 +1,11 @@
-﻿--IF db_id('VitalChoice.Ecommerce') is not null
---BEGIN
---	IF OBJECT_ID(N'[VitalChoice.Ecommerce].[dbo].[Users]', N'U') IS NULL
---	BEGIN
---		IF NOT EXISTS (SELECT [Id] FROM [VitalChoice.Ecommerce].[dbo].[Users])
---		BEGIN
---			INSERT INTO [dbo].[Users]
---			SELECT [Id] FROM [dbo].[AspNetUsers]
---		END
---	END
---END
+﻿IF db_id('VitalChoice.Ecommerce') is not null
+BEGIN
+	--IF OBJECT_ID(N'[VitalChoice.Ecommerce].[dbo].[Users]', N'U') IS NULL
+	--BEGIN
+		IF NOT EXISTS (SELECT [Id] FROM [VitalChoice.Ecommerce].[dbo].[Users])
+		BEGIN
+			INSERT INTO [dbo].[Users]
+			SELECT [Id] FROM [dbo].[AspNetUsers]
+		END
+	--END
+END
