61cdb1cf046d2eddfe7e90e8d0b232547522361e
Aliaksandr Kukrash <multiarc@gmail.com>
2015-07-14 14:42:32 +0300

    Tests

---
+++ test/VitalChoice.Tests/Mapping/Entities/TestEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VitalChoice.Domain.Entities.eCommerce.Base;
+
+namespace VitalChoice.Tests.Mapping.Entities
+{
+    public class TestEntity : DynamicDataEntity<TestEntityOptionValue, TestEntityOptionType>
+    {
+    }
+}
---
+++ test/VitalChoice.Tests/Mapping/Entities/TestEntityOptionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VitalChoice.Domain.Entities.eCommerce.Base;
+
+namespace VitalChoice.Tests.Mapping.Entities
+{
+    public class TestEntityOptionType : OptionType
+    {
+    }
+}
