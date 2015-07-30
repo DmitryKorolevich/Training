a3e3e15035551bae7e512fec30b0862ac0ebda0e
exs007 <007exs@gmail.com>
2015-07-30 20:52:57 +0300

    Angular masked input for phone inputs

---
+++ src/VC.Admin/App/core/utils/parts/maskDirective.js
@@ -0,0 +1,24 @@
+﻿angular.module('app.core.utils.parts.maskDirective', [])
+   .directive('mask', ['$parse', '$timeout', function ($parse,$timeout) {
+       return {
+           restrict: 'A',
+           require: 'ngModel',
+           link: function (scope, elem, attr, modelCtrl) {
+               if (attr.mask)
+               {
+                   modelCtrl.$render = function () {
+                       elem.val(modelCtrl.$viewValue || '');
+                       elem.mask(attr.mask, { placeholder: attr.maskPlaceholder });
+                   };
+
+                   if(attr.clean)
+                   {
+                       modelCtrl.$parsers.push(function (inputValue) {
+                           var resultValues =inputValue.replace('(', '').replace(')', '').replace(' ', '').replace('-', '').replace('x', '');
+                           return resultValues;
+                       });
+                   }
+               }
+           }
+       };
+   }]);
--- src/VC.Admin/App/core/utils/utils.js
+++ src/VC.Admin/App/core/utils/utils.js
@@ -25,6 +25,7 @@ angular.module('app.utils', [
 	'app.core.utils.parts.imgLoadDirective',
 	'app.core.utils.parts.fileFieldDirective',
 	'app.core.utils.parts.numbersOnlyDirective',
+	'app.core.utils.parts.maskDirective',
 	'app.core.utils.textAngular',
 	'app.core.utils.webStorageUtil',
 	'app.core.utils.dataStateRemediator'
