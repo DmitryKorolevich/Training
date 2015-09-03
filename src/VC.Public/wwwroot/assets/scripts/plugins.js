e915aafb454ec260e8112e1a7ec6e768e3a155db
DmitryKorolevich <dmitry.korolevich@gmail.com>
2015-09-04 01:58:21 +0300

    public - fixes in markup for ie8

--- src/VC.Public/wwwroot/assets/scripts/plugins.js
+++ src/VC.Public/wwwroot/assets/scripts/plugins.js
@@ -22,3 +22,27 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+(function ($) {
+	$.support.placeholder = ('placeholder' in document.createElement('input'));
+})(jQuery);
+
+
+//fix for IE7 and IE8
+$(function () {
+	if (!$.support.placeholder) {
+		$("[placeholder]").focus(function () {
+			if ($(this).val() == $(this).attr("placeholder")) $(this).val("");
+		}).blur(function () {
+			if ($(this).val() == "") $(this).val($(this).attr("placeholder"));
+		}).blur();
+
+		$("[placeholder]").parents("form").submit(function () {
+			$(this).find('[placeholder]').each(function () {
+				if ($(this).val() == $(this).attr("placeholder")) {
+					$(this).val("");
+				}
+			});
+		});
+	}
+});
