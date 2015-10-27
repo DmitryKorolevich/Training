76d04d6e6b9e228eeaa8765ce2fa1f9287e6bc32
exs007 <007exs@gmail.com>
2015-10-27 11:10:57 +0300

    Sql identity corrections

--- src/VitalChoice.Database.Infrastructure/PostDeployment/Scripts/UpdateIdentityWithStorefront.sql
+++ src/VitalChoice.Database.Infrastructure/PostDeployment/Scripts/UpdateIdentityWithStorefront.sql
@@ -8,27 +8,27 @@ END
 
 GO
 
-IF ('YES' =(select TOP 1 IS_NULLABLE from INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME ='AspNetUsers' and COLUMN_NAME ='IdUserType'))
-BEGIN
+--IF ('YES' =(select TOP 1 IS_NULLABLE from INFORMATION_SCHEMA.COLUMNS 
+--WHERE TABLE_NAME ='AspNetUsers' and COLUMN_NAME ='IdUserType'))
+--BEGIN
 
-	UPDATE AspNetUsers
-	SET IdUserType=1
-	WHERE IsAdminUser=1
+--	UPDATE AspNetUsers
+--	SET IdUserType=1
+--	WHERE IsAdminUser=1
 
-	UPDATE AspNetUsers
-	SET IdUserType=2
-	WHERE IsAdminUser=0
+--	UPDATE AspNetUsers
+--	SET IdUserType=2
+--	WHERE IsAdminUser=0
 
-	ALTER TABLE [dbo].[AspNetUsers]
-	ALTER COLUMN [IdUserType] INT NOT NULL
+--	ALTER TABLE [dbo].[AspNetUsers]
+--	ALTER COLUMN [IdUserType] INT NOT NULL
 
-	ALTER TABLE [dbo].[AspNetUsers]
-	DROP COLUMN [IsAdminUser]
+--	ALTER TABLE [dbo].[AspNetUsers]
+--	DROP COLUMN [IsAdminUser]
 
-END
+--END
 
-GO
+--GO
 
 IF COL_LENGTH('[dbo].[AspNetRoles]','IdUserType') IS NULL
 BEGIN
@@ -40,33 +40,33 @@ END
 
 GO
 
-IF ('YES' =(select TOP 1 IS_NULLABLE from INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME ='AspNetRoles' and COLUMN_NAME ='IdUserType'))
-BEGIN
+--IF ('YES' =(select TOP 1 IS_NULLABLE from INFORMATION_SCHEMA.COLUMNS 
+--WHERE TABLE_NAME ='AspNetRoles' and COLUMN_NAME ='IdUserType'))
+--BEGIN
 
-	UPDATE AspNetRoles
-	SET IdUserType=1
-	WHERE IsStorefrontRole=0
+--	UPDATE AspNetRoles
+--	SET IdUserType=1
+--	WHERE IsStorefrontRole=0
 
-	UPDATE AspNetRoles
-	SET IdUserType=2
-	WHERE IsStorefrontRole=1
+--	UPDATE AspNetRoles
+--	SET IdUserType=2
+--	WHERE IsStorefrontRole=1
 	
-	ALTER TABLE [dbo].[AspNetRoles]
-	ALTER COLUMN [IdUserType] INT NOT NULL
+--	ALTER TABLE [dbo].[AspNetRoles]
+--	ALTER COLUMN [IdUserType] INT NOT NULL
 
-	ALTER TABLE [dbo].[AspNetRoles] DROP CONSTRAINT [DF_AspNetRoles_IsStorefrontRole]
+--	ALTER TABLE [dbo].[AspNetRoles] DROP CONSTRAINT [DF_AspNetRoles_IsStorefrontRole]
 
-	ALTER TABLE [dbo].[AspNetRoles]
-	DROP COLUMN [IsStorefrontRole]
+--	ALTER TABLE [dbo].[AspNetRoles]
+--	DROP COLUMN [IsStorefrontRole]
 
-	UPDATE AspNetRoles
-	SET IdUserType=3
-	WHERE Name='Affiliate'
+--	UPDATE AspNetRoles
+--	SET IdUserType=3
+--	WHERE Name='Affiliate'
 
-END
+--END
 
-GO
+--GO
 
 IF NOT EXISTS(SELECT * FROM AspNetRoleClaims WHERE ClaimType='AffiliateRole')
 BEGIN
