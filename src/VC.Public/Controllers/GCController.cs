3883b98e0980224f8b7963e59cd94625ff8b665e
exs007 <007exs@gmail.com>
2015-11-17 19:09:32 +0300

    GC check balance form

---
+++ src/VC.Public/Controllers/GCController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using VC.Public.ModelConverters;
+using VC.Public.Models.Auth;
+using VC.Public.Models.Profile;
+using VitalChoice.Core.Base;
+using VitalChoice.Core.Infrastructure;
+using VitalChoice.Domain.Constants;
+using VitalChoice.Domain.Entities;
+using VitalChoice.Domain.Entities.eCommerce.Addresses;
+using VitalChoice.Domain.Entities.eCommerce.Customers;
+using VitalChoice.Domain.Entities.eCommerce.Payment;
+using VitalChoice.Domain.Entities.Options;
+using VitalChoice.Domain.Entities.Users;
+using VitalChoice.Domain.Exceptions;
+using VitalChoice.Domain.Transfer.Base;
+using VitalChoice.Domain.Transfer.Orders;
+using VitalChoice.Domain.Transfer.Products;
+using VitalChoice.DynamicData.Entities;
+using VitalChoice.DynamicData.Interfaces;
+using VitalChoice.Interfaces.Services.Customers;
+using VitalChoice.Interfaces.Services.Orders;
+using VitalChoice.Interfaces.Services.Products;
+using VitalChoice.Interfaces.Services.Users;
+using VitalChoice.Validation.Models;
+using VC.Public.Models.GC;
+
+namespace VC.Public.Controllers
+{
+	[CustomerAuthorize]
+	public class GCController : BaseMvcController
+	{
+		private readonly IHttpContextAccessor _contextAccessor;
+		private readonly IGcService _gcService;
+
+		public GCController(IHttpContextAccessor contextAccessor, IGcService gcService)
+		{
+			_contextAccessor = contextAccessor;
+            _gcService = gcService;
+		}
+
+        [HttpGet]
+        public async Task<Result<GCInfoModel>> GetGCInfo(string id)
+        {
+            GCInfoModel toReturn = null;
+            if (!String.IsNullOrEmpty(id))
+            {
+                GCFilter filter = new GCFilter();
+                filter.Code = id;
+                filter.Paging.PageItemCount = 1;
+                var gc = (await _gcService.GetGiftCertificatesAsync(filter)).Items.FirstOrDefault();
+                if(gc!=null)
+                {
+                    toReturn = new GCInfoModel(gc);
+                }
+            }
+            return toReturn;
+        }
+    }
+}
---
+++ src/VC.Public/wwwroot/app/modules/profile/gccheckform.js
@@ -0,0 +1,76 @@
+﻿$(function ()
+{
+    function getGCInfo(code, successCallback, errorCallback)
+    {
+        $.ajax({
+            url: "/GC/GetGCInfo/"+code,
+            dataType: "json"
+        }).success(function (result)
+        {
+            if (successCallback)
+            {
+                successCallback(result);
+            }
+        }).error(function (result)
+        {
+            if (errorCallback)
+            {
+                errorCallback(result);
+            }
+        });
+    }
+
+    $(document).ready(function ()
+    {
+        var root = $('.gc-checker');
+        if (root.length > 0)
+        {
+            root.find('input[type=button]').click(function() {
+                var code = root.find('input[type=text]').val();
+                root.find('.info').hide();
+                getGCInfo(code, function (result)
+                {
+                    if(result.Data)
+                    {
+                        if(result.Data.IsActive)
+                        {
+                            root.find('.active').show();
+                            root.find('.inactive').hide();
+                            root.find('.status').text('ACTIVE');
+                        }
+                        else
+                        {
+                            root.find('.active').hide();
+                            root.find('.inactive').show();
+                            root.find('.status').text('INACTIVE');
+                        }
+                        root.find('.code').show();
+                        root.find('.amount').show();
+                        root.find('.code strong').text(result.Data.Code);
+                        root.find('.amount strong').text(result.Data.AmountLine);
+                    }
+                    else
+                    {
+                        root.find('.active').hide();
+                        root.find('.inactive').show();
+                        root.find('.status').text('Gift Certificate Not Found');
+                        root.find('.code').hide();
+                        root.find('.amount').hide();
+                        root.find('.code strong').text('');
+                        root.find('.amount strong').text('');
+                    }
+                    root.find('.info').show();
+                },
+                function (result)
+                {
+                    var text = null;
+                    if (result.Messages)
+                    {
+                        text = result.Messages.map(function () { return this.Message; });
+                    }
+                    notifyError(text);
+                });
+            });
+        }
+    });
+});
