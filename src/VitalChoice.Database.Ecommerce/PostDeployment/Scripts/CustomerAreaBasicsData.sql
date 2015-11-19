aee4cfa00e68c8574269ddd18dc01005284bd02f
exs007 <007exs@gmail.com>
2015-11-19 10:05:46 +0300

    Sql delete payment correction

--- src/VitalChoice.Database.Ecommerce/PostDeployment/Scripts/CustomerAreaBasicsData.sql
+++ src/VitalChoice.Database.Ecommerce/PostDeployment/Scripts/CustomerAreaBasicsData.sql
@@ -449,8 +449,9 @@ BEGIN
 
 	DELETE [dbo].[CustomerPaymentMethods] WHERE IdObjectType=@id
 	DELETE [dbo].[OrderPaymentMethods] WHERE IdObjectType=@id
-	--DELETE [dbo].[PaymentMethodsToCustomerTypes] WHERE IdPaymentMethod=@id
-	--DELETE [dbo].[PaymentMethods] WHERE Id=@id
+	DELETE [dbo].[CustomersToPaymentMethods] WHERE IdPaymentMethod=@id
+	DELETE [dbo].[PaymentMethodsToCustomerTypes] WHERE IdPaymentMethod=@id
+	DELETE [dbo].[PaymentMethods] WHERE Id=@id
 END
 
 GO
