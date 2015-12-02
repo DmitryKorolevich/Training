6be627a6abc3429ece39fc9b5e83c9f8761cdc68
DmitryKorolevich <dmitry.korolevich@gmail.com>
2015-12-03 00:45:05 +0300

    stupid youtube videos have been fixed. Burn in hell Youtube!

--- src/VC.Public/wwwroot/app/modules/product/productPage.js
+++ src/VC.Public/wwwroot/app/modules/product/productPage.js
@@ -1,4 +1,6 @@
-﻿window.addEventListener("load", function () {
+﻿var loaded = false;
+
+window.addEventListener("load", function () {
 	$("input[name=sku]:first").attr("checked", true);
 
 	$("body").on("click", "#lnkReviewsTab", function () {
@@ -10,8 +12,7 @@
 	});
 
 	$("body").on("click", "#btnVideoClose", function () {
-		$("#btnVideoClose").remove();
-		$('#' + $(".youtube-dialog").attr('aria-describedby')).dialog('destroy').remove;
+		stopVideo();
 		return false;
 	});
 
@@ -69,32 +70,52 @@
 		return false;
 	});
 
-	$('body').on("click", "a[data-video-id]", function () {
-		var youtubeLink = $(this).attr("data-video-id");
-		$("<div>" +
-				"<iframe class='youtube-popup-container' frameborder='0' allowfullscreen='1' title='YouTube video player' " +
-					"src='http://www.youtube.com/embed/" + youtubeLink + "?autoplay=1&iv_load_policy=3&rel=0&showinfo=0&wmode=opaque&enablejsapi=1&origin=http://staging.g2-dg.com/'>" +
-				"</iframe>" +
-			"</div>")
+	$.each([1, 2, 3, 4], function (index, targetNumber) {
+		$("#yPlayer" + targetNumber).wrap("<div id=up" + targetNumber + "></div>")
+
+		$("#up" + targetNumber)
 			.dialog({
-				open: function () {
-					var jCloseButton = $("<a id='btnVideoClose' class='youtube-popup-close' href='#'>" +
-						"	<img src='/assets/images/close_button.png'/>" +
-						"</a>");
-
-					var jYoutube = $(".youtube-dialog");
-
-					jCloseButton.css("top", jYoutube.css("top"));
-					jCloseButton.css("left", jYoutube.offset().left + jYoutube.width());
-
-					jYoutube.before(jCloseButton);
-				},
 				resizable: false,
 				modal: true,
 				dialogClass: "youtube-dialog"
+			}).dialog("close");
+	});
+
+	$('body').on("click", "a[data-video-id]", function () {
+		if (loaded) {
+			var videoId = $(this).attr("data-video-id");
+
+			var index = 0;
+			$.each($("a[data-video-id]"), function (ind, elem) {
+				if ($(elem).attr("data-video-id") == videoId) {
+					index = ind + 1;
+					return;
+				}
 			});
+
+			$('#' + $("#up" + index).parent().attr('aria-describedby')).dialog('open');
+
+			var jCloseButton = $("<a id='btnVideoClose' class='youtube-popup-close' href='#'>" +
+							"	<img src='/assets/images/close_button.png'/>" +
+							"</a>");
+
+			var jYoutube = $("#up" + index).parent();
+
+			jCloseButton.css("top", jYoutube.css("top"));
+			jCloseButton.css("left", jYoutube.offset().left + jYoutube.width());
+
+			jYoutube.before(jCloseButton);
+
+			playVideo(index - 1);
+		}
+
 		return false;
 	});
+
+	var tag = document.createElement('script');
+	tag.src = "http://www.youtube.com/player_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }, false);
 
 function reviewSubmitSuccess() {
@@ -112,11 +133,72 @@ function reviewSubmitError() {
 	notifyError("Server error occured");
 }
 
-function onYouTubeIframeAPIReady() {
-	ytplayer = document.getElementById(".youtube-popup-container");
-	ytplayer.addEventListener("onStateChange", function (event) {
-		if (event.data == 0) {
-			$(".youtube-dialog").dialog("close");
+var players = [];
+
+function onYouTubePlayerAPIReady() {
+	var videoId = $($("a[data-video-id]")[0]).attr("data-video-id");
+
+	players[0] = new YT.Player('yPlayer1', {
+		videoId: videoId,
+		playerVars: { iv_load_policy: 3, rel: 0, showinfo: 0, wmode: 'opaque', enablejsapi: 1, origin: location.origin },
+		events: {
+			'onStateChange': onPlayerStateChange
 		}
 	});
+
+	videoId = $($("a[data-video-id]")[1]).attr("data-video-id");
+
+	players[1] = new YT.Player('yPlayer2', {
+		videoId: videoId,
+		playerVars: { iv_load_policy: 3, rel: 0, showinfo: 0, wmode: 'opaque', enablejsapi: 1, origin: location.origin },
+		events: {
+			'onStateChange': onPlayerStateChange
+		}
+	});
+
+	videoId = $($("a[data-video-id]")[2]).attr("data-video-id");
+
+	players[2] = new YT.Player('yPlayer3', {
+		videoId: videoId,
+		playerVars: { iv_load_policy: 3, rel: 0, showinfo: 0, wmode: 'opaque', enablejsapi: 1, origin: location.origin },
+		events: {
+			'onStateChange': onPlayerStateChange
+		}
+	});
+
+	videoId = $($("a[data-video-id]")[3]).attr("data-video-id");
+
+	players[3] = new YT.Player('yPlayer4', {
+		videoId: videoId,
+		playerVars: { iv_load_policy: 3, rel: 0, showinfo: 0, wmode: 'opaque', enablejsapi: 1, origin: location.origin },
+		events: {
+			'onStateChange': onPlayerStateChange
+		}
+	});
+
+	loaded = true;
+}
+
+function onPlayerStateChange(event) {
+	if (event.data == 0) {
+		stopVideo();
+	}
+}
+function stopVideo() {
+	players[0].stopVideo();
+	players[1].stopVideo();
+	players[2].stopVideo();
+	players[3].stopVideo();
+
+
+	$("#btnVideoClose").remove();
+	$('#' + $("#up" + 1).parent().attr('aria-describedby')).dialog('close');
+	$('#' + $("#up" + 2).parent().attr('aria-describedby')).dialog('close');
+	$('#' + $("#up" + 3).parent().attr('aria-describedby')).dialog('close');
+	$('#' + $("#up" + 4).parent().attr('aria-describedby')).dialog('close');
+}
+
+function playVideo(index) {
+	players[index].seekTo(0);
+	players[index].playVideo();
 }
