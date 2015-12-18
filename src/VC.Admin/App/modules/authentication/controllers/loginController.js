d6c77b32b78cce972be5419b3a1a3475b2e6e692
exs007 <007exs@gmail.com>
2015-12-18 16:05:54 +0300

    UI corrections Affiliate index page for the storefront Alert top section Empty content master template

--- src/VC.Admin/App/modules/authentication/controllers/loginController.js
+++ src/VC.Admin/App/modules/authentication/controllers/loginController.js
@@ -29,7 +29,8 @@ angular.module('app.modules.authentication.controllers.loginController', [])
 
 							infrastructureService.getReferenceData().success(function(res) {
 								if (res.Success) {
-									$rootScope.ReferenceData = res.Data;
+								    $rootScope.ReferenceData = res.Data;
+								    $rootScope.$state.go("index.oneCol.manageOrders");
 								} else {
 									toaster.pop('error', 'Error!', "Unable to refresh reference data");
 								}
