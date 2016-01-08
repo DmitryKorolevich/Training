7480954cbd5489001bc276e2b82dc7fb3e38e516
DmitryKorolevich <dmitry.korolevich@gmail.com>
2016-01-09 01:39:11 +0300

    working on view cart

---
+++ src/VC.Public/Models/Cart/CartSkuModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VC.Public.Models.Cart
+{
+    public class CartSkuModel
+    {
+		public string Code { get; set; }
+
+		public string ProductPageUrl { get; set; }
+
+		public string DisplayName { get; set; }
+
+	    public string IconUrl { get; set; }
+
+	    public bool InStock { get; set; }
+
+	    public decimal? Price { get; set; }
+
+	    public int Quantity { get; set; }
+
+	    public decimal? SubTotal { get; set; }
+    }
+}
---
+++ src/VC.Public/Models/Cart/ViewCartModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VitalChoice.Infrastructure.Domain.Transfer.Shipping;
+
+namespace VC.Public.Models.Cart
+{
+    public class ViewCartModel
+    {
+	    public IList<CartSkuModel> Skus { get; set; }
+
+		public bool ShipAsap { get; set; }
+
+	    public DateTime? ShippingDate { get; set; }
+
+	    public decimal SubTotal { get; set; }
+
+	    public ShippingUpgradeOption? UpgradeOption { get; set; }
+
+	    public decimal ShippingCost { get; set; }
+
+	    public decimal OrderTotal { get; set; }
+
+	    public string PromoCode { get; set; }
+
+	    public string GiftCertificateCode { get; set; }
+    }
+}
