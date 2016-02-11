44e521657d154747db99a1e4ab9c2a43c0e2253b
exs007 <007exs@gmail.com>
2016-02-11 13:40:13 +0300

    UI corrections Order import split logic statuses bug fix

--- src/VitalChoice.Business/Services/Orders/OrderService.cs
+++ src/VitalChoice.Business/Services/Orders/OrderService.cs
@@ -872,9 +872,12 @@ namespace VitalChoice.Business.Services.Orders
                     }
                     else
                     {
-                        item.Order.POrderStatus = item.Order.OrderStatus;
-                        item.Order.NPOrderStatus = item.Order.OrderStatus;
-                        item.Order.OrderStatus = null;
+                        if (context.SplitInfo?.ShouldSplit == true)
+                        {
+                            item.Order.POrderStatus = item.Order.OrderStatus;
+                            item.Order.NPOrderStatus = item.Order.OrderStatus;
+                            item.Order.OrderStatus = null;
+                        }
                     }
                 }
                 else
