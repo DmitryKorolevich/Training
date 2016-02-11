f5f55dd585f7412ec8ef5cba4a6408f4c1712746
Aliaksandr Kukrash <multiarc@gmail.com>
2016-02-11 21:46:30 +0300

    Log fixes to SQL Express

--- src/VitalChoice.Ecommerce/Context/LogsContext.cs
+++ src/VitalChoice.Ecommerce/Context/LogsContext.cs
@@ -18,18 +18,18 @@ namespace VitalChoice.Ecommerce.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            var connectionString = (new SqlConnectionStringBuilder
-            {
-                DataSource = @"(LocalDB)\MSSQLLocalDB",
-                // TODO: Currently nested queries are run while processing the results of outer queries
-                // This either requires MARS or creation of a new connection for each query. Currently using
-                // MARS since cloning connections is known to be problematic.
-                MultipleActiveResultSets = true,
-                AttachDBFilename = Options.Value.LogPath + "\\Logs.mdf",
-                IntegratedSecurity = true,
-                ConnectTimeout = 60
-            }).ConnectionString;
-            builder.UseSqlServer(connectionString);
+            //var connectionString = (new SqlConnectionStringBuilder
+            //{
+            //    DataSource = @"(LocalDB)\MSSQLLocalDB",
+            //    // TODO: Currently nested queries are run while processing the results of outer queries
+            //    // This either requires MARS or creation of a new connection for each query. Currently using
+            //    // MARS since cloning connections is known to be problematic.
+            //    MultipleActiveResultSets = true,
+            //    AttachDBFilename = Options.Value.LogPath + "\\Logs.mdf",
+            //    IntegratedSecurity = true,
+            //    ConnectTimeout = 60
+            //}).ConnectionString;
+            builder.UseSqlServer("Data Source=.\\;Integrated Security=true;initial catalog=Logs;");
 
             base.OnConfiguring(builder);
         }
