f58ed537b81f406d0397f2a5dcd23d000c13e03c
Aliaksandr Kukrash <multiarc@gmail.com>
2016-02-26 00:53:35 +0300

    *optimization: add foreign key validity check

--- src/VitalChoice.Caching/Services/Cache/EntityInternalCache.cs
+++ src/VitalChoice.Caching/Services/Cache/EntityInternalCache.cs
@@ -479,7 +479,7 @@ namespace VitalChoice.Caching.Services.Cache
 
             foreach (var foreignKey in foreignKeys)
             {
-                if (CacheFactory.CacheExist(foreignKey.Key.DependentType))
+                if (foreignKey.Value.IsValid && CacheFactory.CacheExist(foreignKey.Key.DependentType))
                 {
                     var cache = CacheFactory.GetCache(foreignKey.Key.DependentType);
                     var collectionForeignKey = foreignKey.Key as EntityForeignKeyCollectionInfo;
