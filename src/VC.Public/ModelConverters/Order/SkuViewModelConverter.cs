0ea7426e62b27538fd9a8e7b0bc37e1ddb42f78e
Aliaksandr Kukrash <multiarc@gmail.com>
2016-04-04 21:09:44 +0300

    vc_new-577 buying gift certificates and e-gift certificates on the storefront doesn't work - fixed

--- src/VC.Public/ModelConverters/Order/SkuViewModelConverter.cs
+++ src/VC.Public/ModelConverters/Order/SkuViewModelConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using VC.Public.Models.Cart;
 using VitalChoice.DynamicData.Base;
+using VitalChoice.Ecommerce.Domain.Entities.Products;
 using VitalChoice.Infrastructure.Domain.Dynamic;
 
 namespace VC.Public.ModelConverters.Order
@@ -9,7 +10,8 @@ namespace VC.Public.ModelConverters.Order
     {
         public override void DynamicToModel(CartSkuModel model, SkuDynamic dynamic)
         {
-            model.InStock = ((bool?)dynamic.SafeData.DisregardStock ?? false) || ((int?)dynamic.SafeData.Stock ?? 0) > 0;
+            model.InStock = dynamic.IdObjectType == (int) ProductType.EGс || dynamic.IdObjectType == (int) ProductType.Gc ||
+                            ((bool?) dynamic.SafeData.DisregardStock ?? false) || ((int?) dynamic.SafeData.Stock ?? 0) > 0;
         }
 
         public override void ModelToDynamic(CartSkuModel model, SkuDynamic dynamic)
