3efe926518bdd76ea1ddc693f5d2f2524a5707f1
Aliaksandr Kukrash <multiarc@gmail.com>
2016-04-29 14:35:29 +0300

    Content move scripts update

--- sql/ContentMove/articles_update_invalid_image_links.sql
+++ sql/ContentMove/articles_update_invalid_image_links.sql
@@ -1,5 +1,5 @@
-UPDATE ContentItems
-SET Description = REPLACE(REPLACE(REPLACE(c.Description, 'http://www.vitalchoice.com/shop/pc/catalog/', '/files/catalog/'), '/shop/pc/catalog/', '/files/catalog/'), '../pc/catalog/', '/files/catalog/') 
-FROM ContentItems AS c
-INNER JOIN Articles AS a ON a.ContentItemId = c.Id
-WHERE c.Description LIKE '%/catalog/%'
+UPDATE Articles
+--SET Description = REPLACE(REPLACE(REPLACE(c.Description, 'http://www.vitalchoice.com/shop/pc/catalog/', '/files/catalog/'), '/shop/pc/catalog/', '/files/catalog/'), '../pc/catalog/', '/files/catalog/') 
+SET FileUrl = REPLACE(REPLACE(REPLACE(c.FileUrl, 'http://www.vitalchoice.com/shop/pc/catalog/', '/files/catalog/'), '/shop/pc/catalog/', '/files/catalog/'), '../pc/catalog/', '/files/catalog/')
+FROM Articles AS c
+WHERE c.FileUrl LIKE '%/catalog/%'
