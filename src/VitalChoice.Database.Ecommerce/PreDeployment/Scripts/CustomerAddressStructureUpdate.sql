0b70db42efb0ebc83d8fad64f63846f86f89503d
Aliaksandr Kukrash <multiarc@gmail.com>
2016-06-03 13:59:49 +0300

    Index fix

--- src/VitalChoice.Database.Ecommerce/PreDeployment/Scripts/CustomerAddressStructureUpdate.sql
+++ src/VitalChoice.Database.Ecommerce/PreDeployment/Scripts/CustomerAddressStructureUpdate.sql
@@ -21,7 +21,7 @@ IF NOT EXISTS(SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Cus
 BEGIN
 	CREATE NONCLUSTERED INDEX [IX_Email] ON [dbo].[Customers]
 	(
-		[Email] ASC
+		[Email] ASC, [StatusCode] ASC
 	)
 	INCLUDE ( 	[Id],
 		[IdObjectType],
@@ -29,7 +29,6 @@ BEGIN
 		[DateEdited],
 		[IdEditedBy],
 		[IdDefaultPaymentMethod],
-		[StatusCode],
 		[PublicId],
 		[IdAffiliate],
 		[IdProfileAddress]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
