ed2b022c6e3ab8538bbe9fcd2ec0e73be1e6871f
Aliaksandr Kukrash <multiarc@gmail.com>
2016-06-03 16:46:30 +0300

    Asp net users index

--- src/VitalChoice.Database.Infrastructure/PreDeployment/Scripts/IdentityIndexes.sql
+++ src/VitalChoice.Database.Infrastructure/PreDeployment/Scripts/IdentityIndexes.sql
@@ -46,3 +46,40 @@ BEGIN
 		[Order]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 END
 GO
+
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[AspNetUsers]') AND name = N'IX_UserTypeDeletedDate')
+BEGIN
+	CREATE NONCLUSTERED INDEX [IX_UserTypeDeletedDate] ON [dbo].[AspNetUsers]
+	(
+		[DeletedDate] ASC,
+		[IdUserType] ASC
+	)
+	INCLUDE ( 	[Id],
+		[PublicId],
+		[AccessFailedCount],
+		[ConcurrencyStamp],
+		[Email],
+		[EmailConfirmed],
+		[UserName],
+		[FirstName],
+		[LastName],
+		[Status],
+		[LockoutEnabled],
+		[LockoutEnd],
+		[NormalizedEmail],
+		[NormalizedUserName],
+		[PasswordHash],
+		[PhoneNumber],
+		[PhoneNumberConfirmed],
+		[LastLoginDate],
+		[CreateDate],
+		[UpdatedDate],
+		[SecurityStamp],
+		[TwoFactorEnabled],
+		[ConfirmationToken],
+		[TokenExpirationDate],
+		[IsConfirmed]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+END
+GO
+
+
