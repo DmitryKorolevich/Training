c88fb07b8612959a320835402abe757df11bd08e
exs007 <007exs@gmail.com>
2016-06-07 19:16:49 +0300

    Sql correction

--- src/VitalChoice.Database.Ecommerce/PreDeployment/Scripts/CustomerAddressStructureUpdate.sql
+++ src/VitalChoice.Database.Ecommerce/PreDeployment/Scripts/CustomerAddressStructureUpdate.sql
@@ -50,7 +50,6 @@ BEGIN
 		[StatusCode],
 		[PublicId],
 		[IdAffiliate],
-		[IdProfileAddress],
 		Email) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 END
 GO
