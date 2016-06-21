59456c5b6bc7aaf67ead19d58368573d9590881c
Aliaksandr Kukrash <multiarc@gmail.com>
2016-06-21 18:14:25 +0300

    Remote payment info update

--- src/VitalChoice.DynamicData/Base/DynamicMapper.cs
+++ src/VitalChoice.DynamicData/Base/DynamicMapper.cs
@@ -486,7 +486,6 @@ namespace VitalChoice.DynamicData.Base
             {
                 throw new ApiException($"UpdateEntityItem<{typeof (TEntity)}> have no OptionTypes, are you forgot to pass them?");
             }
-            entity.MappedObject = dynamic;
             FillEntityOptions(dynamic, entity.OptionTypes, entity);
             entity.DateEdited = DateTime.Now;
             entity.StatusCode = dynamic.StatusCode;
@@ -572,6 +571,7 @@ namespace VitalChoice.DynamicData.Base
 
         private static void FillEntityOptions(TDynamic obj, ICollection<TOptionType> optionTypes, TEntity entity)
         {
+            entity.MappedObject = obj;
             HashSet<int> optionTypeIds = new HashSet<int>(optionTypes.Select(o => o.Id));
             entity.OptionValues.RemoveAll(o => !optionTypeIds.Contains(o.IdOptionType));
             Dictionary<int, TOptionValue> optionValues = entity.OptionValues.ToDictionary(v => v.IdOptionType);
