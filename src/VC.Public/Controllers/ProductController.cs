ed17150b8846be86ba791c9ac5d7c49118e10565
Aliaksandr Kukrash <multiarc@gmail.com>
2016-09-01 14:51:42 +0300

    Fix review unexpected errors

--- src/VC.Public/Controllers/ProductController.cs
+++ src/VC.Public/Controllers/ProductController.cs
@@ -21,62 +21,62 @@ namespace VC.Public.Controllers
 {
     public class ProductController : BaseContentController
     {
-	    private readonly ICategoryViewService _categoryViewService;
-	    private readonly IProductViewService _productViewService;
-	    private readonly ReCaptchaValidator _reCaptchaValidator;
-	    private readonly IProductService _productService;
+        private readonly ICategoryViewService _categoryViewService;
+        private readonly IProductViewService _productViewService;
+        private readonly ReCaptchaValidator _reCaptchaValidator;
+        private readonly IProductService _productService;
         private readonly IProductCategoryService _productCategoryService;
         private readonly IProductReviewService _productReviewService;
 
-	    public ProductController(ICategoryViewService categoryViewService, 
-            IProductViewService productViewService, 
-            ReCaptchaValidator reCaptchaValidator, 
+        public ProductController(ICategoryViewService categoryViewService,
+            IProductViewService productViewService,
+            ReCaptchaValidator reCaptchaValidator,
             IProductService productService,
             IProductCategoryService productCategoryService,
             IProductReviewService productReviewService)
         {
-		    _categoryViewService = categoryViewService;
-		    _productViewService = productViewService;
-		    _reCaptchaValidator = reCaptchaValidator;
-		    _productService = productService;
-	        _productCategoryService = productCategoryService;
+            _categoryViewService = categoryViewService;
+            _productViewService = productViewService;
+            _reCaptchaValidator = reCaptchaValidator;
+            _productService = productService;
+            _productCategoryService = productCategoryService;
             _productReviewService = productReviewService;
-	    }
+        }
 
-	    private async Task PopulateProductReviewAssets(Guid id)
-	    {
-			var productTransfer = await _productService.SelectTransferAsync(id, true);
+        private async Task PopulateProductReviewAssets(Guid id)
+        {
+            var productTransfer = await _productService.SelectTransferAsync(id, true);
 
-			ViewBag.Image = productTransfer.ProductDynamic.SafeData.Thumbnail;
-			ViewBag.Name = productTransfer.ProductDynamic.Name;
-			ViewBag.SubTitle = productTransfer.ProductDynamic.SafeData.SubTitle;
-		}
+            ViewBag.Image = productTransfer.ProductDynamic.SafeData.Thumbnail;
+            ViewBag.Name = productTransfer.ProductDynamic.Name;
+            ViewBag.SubTitle = productTransfer.ProductDynamic.SafeData.SubTitle;
+        }
 
-	    private async Task<PagedListEx<ReviewListItemModel>> GetReviewsModel(ProductReviewFilter filter)
-	    {
-			filter.Sorting.SortOrder = FilterSortOrder.Desc;
-		    filter.Sorting.Path = ProductReviewSortPath.DateCreated;
+        private async Task<PagedListEx<ReviewListItemModel>> GetReviewsModel(ProductReviewFilter filter)
+        {
+            filter.Sorting.SortOrder = FilterSortOrder.Desc;
+            filter.Sorting.Path = ProductReviewSortPath.DateCreated;
 
-			var productReviews = await _productReviewService.GetProductReviewsAsync(filter);
+            var productReviews = await _productReviewService.GetProductReviewsAsync(filter);
 
-			var reviewsModel = new PagedListEx<ReviewListItemModel>
-			{
-				Items = productReviews.Items.Select(p => new ReviewListItemModel()
-				{
-					DateCreated = p.DateCreated,
-					Title = p.Title,
-					Review = p.Description,
-					CustomerName = p.CustomerName,
-					Rating = p.Rating,
-				}).ToList(),
-				Count = productReviews.Count,
-				Index = filter.Paging.PageIndex
-			};
+            var reviewsModel = new PagedListEx<ReviewListItemModel>
+            {
+                Items = productReviews.Items.Select(p => new ReviewListItemModel()
+                {
+                    DateCreated = p.DateCreated,
+                    Title = p.Title,
+                    Review = p.Description,
+                    CustomerName = p.CustomerName,
+                    Rating = p.Rating,
+                }).ToList(),
+                Count = productReviews.Count,
+                Index = filter.Paging.PageIndex
+            };
 
-		    return reviewsModel;
-	    }
+            return reviewsModel;
+        }
 
-	    [HttpGet]
+        [HttpGet]
         public async Task<IActionResult> Categories()
         {
             var toReturn = await _categoryViewService.GetContentAsync(ControllerContext, User);
@@ -88,7 +88,7 @@ namespace VC.Public.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> CategoryByIdOld([FromQuery]string idCategory)
+        public async Task<IActionResult> CategoryByIdOld([FromQuery] string idCategory)
         {
             int idold;
             if (Int32.TryParse(idCategory, out idold))
@@ -119,7 +119,7 @@ namespace VC.Public.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> ProductByIdOld([FromQuery]string idproduct, [FromQuery]string idcategory)
+        public async Task<IActionResult> ProductByIdOld([FromQuery] string idproduct, [FromQuery] string idcategory)
         {
             int idOldProduct;
             string url = null;
@@ -128,7 +128,7 @@ namespace VC.Public.Controllers
                 var item = await _productService.SelectTransferByIdOldAsync(idOldProduct);
                 if (!string.IsNullOrEmpty(item?.ProductContent?.Url))
                 {
-                    url =$"/product/{item.ProductContent.Url}";
+                    url = $"/product/{item.ProductContent.Url}";
 
                     int idOldCategory;
                     if (Int32.TryParse(idcategory, out idOldCategory))
@@ -150,60 +150,60 @@ namespace VC.Public.Controllers
         }
 
         [HttpGet]
-		public async Task<IActionResult> Product(string url)
-		{
-			var toReturn = await _productViewService.GetContentAsync(ControllerContext, User);
+        public async Task<IActionResult> Product(string url)
+        {
+            var toReturn = await _productViewService.GetContentAsync(ControllerContext, User);
             if (toReturn != null)
-			{
-				return View("~/Views/Content/ProductPage.cshtml", new ContentPageViewModel(toReturn));
-			}
-			return BaseNotFoundView();
-		}
+            {
+                return View("~/Views/Content/ProductPage.cshtml", new ContentPageViewModel(toReturn));
+            }
+            return BaseNotFoundView();
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> AddReview(Guid id)
-		{
-			await PopulateProductReviewAssets(id);
+        [HttpGet]
+        public async Task<IActionResult> AddReview(Guid id)
+        {
+            await PopulateProductReviewAssets(id);
 
-			return PartialView("_AddReview", new AddReviewModel {ProductId = id});
-		}
+            return PartialView("_AddReview", new AddReviewModel {ProductId = id});
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> AddReview(AddReviewModel model)
-		{
-			if (Validate(model))
-			{
-				if (!await _reCaptchaValidator.Validate(Request.Form[ReCaptchaValidator.DefaultPostParamName]))
-				{
-					ModelState.AddModelError(string.Empty, ErrorMessagesLibrary.Data[ErrorMessagesLibrary.Keys.WrongCaptcha]);
-				}
-				else
-				{
-					await _productReviewService.UpdateProductReviewAsync(new ProductReview()
-					{
-						IdProduct = await _productService.GetProductInternalIdAsync(model.ProductId),
-						StatusCode = RecordStatusCode.NotActive,
-						CustomerName = model.CustomerName?.Trim(),
-						Email = model.Email?.Trim(),
-						Title = model.ReviewTitle?.Trim(),
-						Rating = model.Rating,
-						Description = model.Review
-					});
+        [HttpPost]
+        public async Task<IActionResult> AddReview(AddReviewModel model)
+        {
+            if (Validate(model))
+            {
+                if (!await _reCaptchaValidator.Validate(Request.Form[ReCaptchaValidator.DefaultPostParamName]))
+                {
+                    ModelState.AddModelError(string.Empty, ErrorMessagesLibrary.Data[ErrorMessagesLibrary.Keys.WrongCaptcha]);
+                }
+                else
+                {
+                    await _productReviewService.UpdateProductReviewAsync(new ProductReview()
+                    {
+                        IdProduct = await _productService.GetProductInternalIdAsync(model.ProductId),
+                        StatusCode = RecordStatusCode.NotActive,
+                        CustomerName = model.CustomerName?.Trim(),
+                        Email = model.Email?.Trim(),
+                        Title = model.ReviewTitle?.Trim(),
+                        Rating = model.Rating,
+                        Description = model.Review
+                    });
 
-					ViewBag.SuccessMessage = InfoMessagesLibrary.Data[InfoMessagesLibrary.Keys.EntitySuccessfullyAdded];
-					return PartialView("_AddReviewInner", model);
-				}
-			}
+                    ViewBag.SuccessMessage = InfoMessagesLibrary.Data[InfoMessagesLibrary.Keys.EntitySuccessfullyAdded];
+                    return PartialView("_AddReviewInner", model);
+                }
+            }
 
-			return PartialView("_AddReviewInner", model);
-		}
+            return PartialView("_AddReviewInner", model);
+        }
 
         [HttpGet]
         public async Task<IActionResult> AddOutOfStockProductRequest(Guid id)
         {
             await PopulateProductReviewAssets(id);
 
-            return PartialView("_AddOutOfStockProductRequest", new AddOutOfStockProductRequestModel { ProductId = id });
+            return PartialView("_AddOutOfStockProductRequest", new AddOutOfStockProductRequestModel {ProductId = id});
         }
 
         [HttpPost]
@@ -221,7 +221,7 @@ namespace VC.Public.Controllers
                     {
                         IdProduct = await _productService.GetProductInternalIdAsync(model.ProductId),
                         Name = model.Name,
-                        Email=model.Email
+                        Email = model.Email
                     });
 
                     ViewBag.SuccessMessage = InfoMessagesLibrary.Data[InfoMessagesLibrary.Keys.EntitySuccessfullyAdded];
@@ -234,30 +234,35 @@ namespace VC.Public.Controllers
 
 
         [HttpGet]
-	    public async Task<IActionResult> FullReviews(string url, int pageNumber = 1)
-	    {
-		    var transfer = await _productService.SelectTransferAsync(url, true);
+        public async Task<IActionResult> FullReviews(string url, int pageNumber = 1)
+        {
+            var transfer = await _productService.SelectTransferAsync(url, true);
 
-		    var reviewsModel = await GetReviewsModel(new ProductReviewFilter()
-		    {
-			    IdProduct = transfer.ProductDynamic.Id,
-			    Paging = new Paging()
-			    {
-				    PageIndex = pageNumber,
-				    PageItemCount = BaseAppConstants.DEFAULT_LIST_TAKE_COUNT
-			    },
-			    StatusCode = RecordStatusCode.Active
-		    });
+            if (transfer?.ProductDynamic != null)
+            {
 
-		    return View(new FullReviewsModel()
-		    {
-				ProductPublicId = transfer.ProductDynamic.PublicId,
-				ProductTitle = transfer.ProductDynamic.Name,
-				ProductSubTitle = transfer.ProductDynamic.SafeData.SubTitle,
-				ProductUrl = transfer.ProductContent.Url,
-				Reviews = reviewsModel,
-				AverageRatings = await _productReviewService.GetApprovedAverageRatingsAsync(transfer.ProductDynamic.Id)
-			});
-	    }
-	}
+                var reviewsModel = await GetReviewsModel(new ProductReviewFilter()
+                {
+                    IdProduct = transfer.ProductDynamic.Id,
+                    Paging = new Paging()
+                    {
+                        PageIndex = pageNumber,
+                        PageItemCount = BaseAppConstants.DEFAULT_LIST_TAKE_COUNT
+                    },
+                    StatusCode = RecordStatusCode.Active
+                });
+
+                return View(new FullReviewsModel()
+                {
+                    ProductPublicId = transfer.ProductDynamic.PublicId,
+                    ProductTitle = transfer.ProductDynamic.Name,
+                    ProductSubTitle = transfer.ProductDynamic.SafeData.SubTitle,
+                    ProductUrl = transfer.ProductContent.Url,
+                    Reviews = reviewsModel,
+                    AverageRatings = await _productReviewService.GetApprovedAverageRatingsAsync(transfer.ProductDynamic.Id)
+                });
+            }
+            return NotFound();
+        }
+    }
 }
