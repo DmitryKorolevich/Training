8690cd3af6b37e018db715de6c8348b804d23eb3
exs007 <007exs@gmail.com>
2016-12-07 17:16:27 +0300

    Correction for vc_new-1308

--- src/VC.Admin/Validators/Affiliate/AffiliateManageModelValidator.cs
+++ src/VC.Admin/Validators/Affiliate/AffiliateManageModelValidator.cs
@@ -115,7 +115,7 @@ namespace VC.Admin.Validators.Affiliate
                     .NotEmpty()
                     .WithMessage(model => model.Zip, ValidationMessages.FieldRequired)
                     .Length(0, BaseAppConstants.ZIP_MAX_SIZE)
-                    .WithMessage(model => model.Zip, ValidationMessages.FieldLength, BaseAppConstants.DEFAULT_TEXT_FIELD_MAX_SIZE);
+                    .WithMessage(model => model.Zip, ValidationMessages.FieldLength, BaseAppConstants.ZIP_MAX_SIZE);
 
                 RuleFor(model => model.Phone)
                     .NotEmpty()
